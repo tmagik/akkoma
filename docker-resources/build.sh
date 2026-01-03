11035a182857f98bc5d46a74bd320425eee03c8f
Troy Benjegerdes <troy@7el.us>
2026-01-03 22:44:12 +0000

    Podman work-in-progress

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
-docker compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) akkoma
-docker compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) db
+if DOCKER=$(command -v docker); then
+	echo "Using docker command: ${DOCKER}"
+elif DOCKER=$(command -v podman); then
+	echo "Using podman command: ${DOCKER}"
+else
+	echo "No viable docker or podman command found"
+	exit 1
+fi
+
+$DOCKER compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) akkoma
+$DOCKER compose build --build-arg UID=$(id -u) --build-arg GID=$(id -g) db
--- docker-resources/manage.sh
+++ docker-resources/manage.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-docker compose run --rm akkoma $@
+if DOCKER=$(command -v docker); then
+	echo "Using docker command: ${DOCKER}"
+elif DOCKER=$(command -v podman); then
+	echo "Using podman command: ${DOCKER}"
+else
+	echo "No viable docker or podman command found"
+	exit 1
+fi
+
+${DOCKER:=docker} compose run --rm akkoma $@
